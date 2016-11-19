917df5ea13ed04657e072af249b7144f28a962e2
Vipul Kanade <vipultechjoy@gmail.com>
2016-11-19 00:44:28 -0800

    Updating Changes

--- cmpe_295_b_Back_End/routes/mqttsubscriber.js
+++ cmpe_295_b_Back_End/routes/mqttsubscriber.js
@@ -1,31 +1,32 @@
-const mqtt = require('mqtt')
-const client =  mqtt.connect('iot.eclipse.org', 1883, 60)
+const mqtt = require('mqtt');
+const client =  mqtt.connect('mqtt://iot.eclipse.org', 1883, 60);
 
-var iotState = ''
-var connected = false
+var iotState = '';
+var connected = false;
 
-var topic1 = ''
-var topic2 = ''
+var topic1 = 'topic/GeneralizedIoT';
+var topic2 = '';
 
-client.on('connect' () => {
-	client.subscribe(topic1)
-	client.subscribe(topic2)
-})
+client.on('connect', function () {
+	client.subscribe(topic1);
+	client.subscribe(topic2);
+});
 
-client.on('message', (topic, message) => {
+client.on('message', function(topic, message) {
 	switch (topic) {
 		case topic1:
-			return handleTopic1(message)
+			return handleTopic1(message);
 		case topic2:
-			return handleTopic2(message)
+			return handleTopic2(message);
 	}
-	console.log('No Handler for topic %s', topic)
-})
+	console.log('No Handler for topic %s', topic);
+});
 
 function handleTopic1 (message) {
-	console.log('%s', message)
+	console.log('%s', message);
 }
 
 function handleTopic2 (message) {
-	console.log('%s', message)
+	console.log('%s', message);
 }
+
